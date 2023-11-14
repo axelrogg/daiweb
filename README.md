a316ffb02003f29c045a0f862c84efa95b0b7cd2
Axel Rodríguez Chang <axelrogg@icloud.com>
2023-11-14 14:54:58 +0100

    batman

---
+++ README.md
@@ -0,0 +1,14 @@
+Página web de la Delegación de Alumnos de la Escuela de Ingeniería Industrial de la Universidad de Vigo
+
+## Primeros pasos
+
+Para iniciar el servidor de desarrollo ejecuta el siguiente comando.
+
+```bash
+bun dev
+```
+
+Abre [http://localhost:3000](http://localhost:3000) en tu navegador (de preferencia Google Chrome)
+para ver el resultado.
+
+Puedes empezar viendo/modificando `app/page.tsx`.
---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Delegación de Alumnos de la Escuela de Ingeniería Industrial",
+    // TODO: Add description
+    // description: '',
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="es">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
