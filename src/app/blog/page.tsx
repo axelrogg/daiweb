6131199e7e260a1f88a7c08e6e3f4d5919d6393c
Axel Rodríguez Chang <axelrogg@icloud.com>
2023-12-02 07:36:06 +0100

    Blog is progressing

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 type BlogEntryListContent = {
     title: string
@@ -9,6 +10,7 @@ type BlogEntryListContent = {
     }
     dateCreated: Date
     sneakPeek: string
+    postUri: string
 }
 
 const blogcont: BlogEntryListContent[] = [
@@ -16,11 +18,12 @@ const blogcont: BlogEntryListContent[] = [
         title: "The Wonders of Technology",
         heroImageUri: "",
         author: {
-            avatarUri: "",
+            avatarUri: "https://media.vanityfair.com/photos/633b42f061a5fa19b93f1255/master/w_1600,c_limit/LGM-20221003-emma-thompson.jpg",
             name: "Emma Thompson",
         },
         dateCreated: new Date(),
-        sneakPeek: "This is a sneak peek"
+        sneakPeek: "This is a sneak peek",
+        postUri: "",
     },
     {
         title: "Exploring the Serene Beauty of Nature",
@@ -30,15 +33,64 @@ const blogcont: BlogEntryListContent[] = [
             name: "Alexander Greene",
         },
         dateCreated: new Date(),
-        sneakPeek: "This is a sneak peek"
+        sneakPeek: "This is a sneak peek",
+        postUri: ""
     }
 ]
+
+function displayDate(date: Date) {
+    // const day = String(date.getUTCDate()).padStart(2, "0");
+    const months = [
+        "Ene",
+        "Feb",
+        "Mar",
+        "Abr",
+        "May",
+        "Jun",
+        "Jul",
+        "Ago",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dic",
+    ];
+    const month = months[date.getUTCMonth()];
+    const year = date.getUTCFullYear();
+    return `${month} ${year}`;
+}
+
 export default function Blog() {
     return (
-        <div className="w-fit px-5">
-            <h1 className="text-6xl font-bold">
-                Blog
+        <div className="flex flex-col">
+            <h1 className="flex justify-center text-6xl font-bold mb-8">
+                El blog de la DAI
             </h1>
+            {
+                blogcont.map(({author, dateCreated, heroImageUri, sneakPeek, title, postUri}, index) => (
+                    <div key={index}>
+                        
+                        {/* TODO: Change both Link's href uris */}
+
+                        <Link href={"/"}>
+                            <div className="my-4 p-4">
+                                <Link className="flex flex-row items-center" href={"/contacto"}>
+                                    <Image
+                                        src={author.avatarUri}
+                                        alt={`${author.name} avatar image`}
+                                        width={30}
+                                        height={30}
+                                        style={{borderRadius: 100, marginRight: 10}}
+                                        />
+                                    <p>{author.name} - {displayDate(dateCreated)}</p>
+                                </Link>
+                                <h2 className="text-2xl font-bold">{title}</h2>
+                                <p>{sneakPeek}</p>
+                            </div>
+                        </Link>
+                        { (index < blogcont.length - 1) && <hr/>}
+                    </div>
+                ))
+            }
         </div>
     )
 }
