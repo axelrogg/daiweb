3e49e4e193a1bdd4e1f881deca9e7a7277bd7630
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-03-26 21:14:55 +0100

    fix: stop footer from overlapping with page content

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,11 +2,32 @@ import { Button } from "@/components/ui/button";
 import { DefaultLayout } from "@/layouts/DefaultLayout";
 import Link from "next/link";
 
+/**
+ * Used by NextJS when a user tries to access a route that doesn't exist.
+ *
+ * The component's height is set using a calculated value to ensure it fits
+ * within the viewport without causing overflow and the need for scrolling.
+ * Initially, `min-h-screen` was employed with the intention of having the
+ * component occupy the full height of the screen. However, this approach
+ * didn't account for the navbar's presence, introduced by wrapping the
+ * component in `DefaultLayout`, which occupies additional space at the top of
+ * the viewport.
+ * 
+ * To address this, the height is explicitly calculated as `100vh - 155px`.
+ * This calculation subtracts the navbar's height from the full viewport height,
+ * allowing the content of the `NotFound` page to be fully visible without
+ * extending beyond the viewport's bounds and triggering a scrollbar. The
+ * adjustment ensures the user can view the entire page content comfortably,
+ * without unnecessary scrolling.
+ *
+ * @see {@link https://nextjs.org/docs/app/api-reference/file-conventions/not-found}
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/calc}
+ */
 export default function NotFound() {
     return (
         <DefaultLayout>
-            <div className="mt-20 flex flex-col items-center justify-center">
-                <h2 className="text-5xl font-bold">404 Not Found</h2>
+            <div className="h-[calc(100dvh-155px)] flex flex-col items-center justify-center w-full">
+                <h2 className="text-4xl font-bold text-center">404 Not Found</h2>
                 <p className="my-5">
                     ¡Ups! No pudimos encontrar lo que buscas.
                 </p>
--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,10 @@
 import { NavBar } from "@/components/navbar";
 
 export const DefaultLayout = ({ children }: { children: React.ReactNode }) => (
-    <div className="min-h-full flex-col px-5">
+    <div className="min-h-dvh flex flex-col px-5">
         <NavBar />
         {children}
-        <footer className="fixed bottom-0 left-0 flex w-full flex-col items-center pb-5 pt-1">
+        <footer className="flex w-full flex-col items-center py-5">
             <p>&copy;2024 DAI - Galicia, España</p>
             <ul className="list-inside list-disc"></ul>
             <ol className="list-inside list-decimal"></ol>
