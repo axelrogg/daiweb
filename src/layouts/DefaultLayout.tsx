75f5618db6c4925160871269745fbf166d630174
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-03-27 08:22:42 +0100

    Remove <ul> and <ol> elements from default footer

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,13 +1,11 @@
 import { NavBar } from "@/components/navbar";
 
 export const DefaultLayout = ({ children }: { children: React.ReactNode }) => (
-    <div className="min-h-dvh flex flex-col px-5">
+    <div className="flex min-h-dvh flex-col px-5">
         <NavBar />
         {children}
         <footer className="flex w-full flex-col items-center py-5">
             <p>&copy;2024 DAI - Galicia, España</p>
-            <ul className="list-inside list-disc"></ul>
-            <ol className="list-inside list-decimal"></ol>
         </footer>
     </div>
 );
