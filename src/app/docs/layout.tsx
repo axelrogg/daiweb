ee2eb8d5d4167ab7311f5012dc51f23ae7d53d99
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-03-27 08:17:27 +0100

    Refactor docs/ layout to its own file, so it can affect all /docs children

---
+++ src/app/docs/layout.tsx
@@ -0,0 +1,5 @@
+import { DefaultLayout } from "@/layouts/DefaultLayout";
+
+export default function DocsLayout({ children }: { children: React.ReactNode }) {
+    return <DefaultLayout>{children}</DefaultLayout>;
+}
--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,35 +1,32 @@
 import { DocsInfoCard } from "@/components/docs-info-card";
-import { DefaultLayout } from "@/layouts/DefaultLayout";
 import { ArrowRightIcon } from "@heroicons/react/24/solid";
 
 export default function Docs() {
     return (
         <>
-            <DefaultLayout>
-                <h1 className="mb-8 text-5xl font-bold">Documentación</h1>
-                <p>
-                    ¡Bienvenidos a la web de la Delegación de Alumnos de
-                    Industriales! En esta sección podrás encontrar información
-                    sobre los servicios que ofrecemos, las formas de acceder a
-                    ellos y mucho más.
-                </p>
-                <div className="my-4 grid grid-flow-col grid-rows-3 gap-4">
-                    <DocsInfoCard
-                        title="Añadir el correo electrónico de la UVigo"
-                        description="Cómo añadir el correo alumnos.uvigo.es a tu ordenador y móvil."
-                        href="/docs/anadir-correo"
-                    >
-                        <ArrowRightIcon className="h-6 w-6 " />
-                    </DocsInfoCard>
-                    <DocsInfoCard
-                        title="Activar el WiFi de la UVigo"
-                        description="Cómo estar siempre conectado en cualquier campus de la universidad"
-                        href="/docs/activar-wifi"
-                    >
-                        <ArrowRightIcon className="h-6 w-6 " />
-                    </DocsInfoCard>
-                </div>
-            </DefaultLayout>
+            <h1 className="mb-8 text-5xl font-bold">Documentación</h1>
+            <p>
+                ¡Bienvenidos a la web de la Delegación de Alumnos de
+                Industriales! En esta sección podrás encontrar información sobre
+                los servicios que ofrecemos, las formas de acceder a ellos y
+                mucho más.
+            </p>
+            <div className="my-4 grid grid-flow-col grid-rows-3 gap-4">
+                <DocsInfoCard
+                    title="Añadir el correo electrónico de la UVigo"
+                    description="Cómo añadir el correo alumnos.uvigo.es a tu ordenador y móvil."
+                    href="/docs/anadir-correo-uvigo"
+                >
+                    <ArrowRightIcon className="h-6 w-6 " />
+                </DocsInfoCard>
+                <DocsInfoCard
+                    title="Activar el WiFi de la UVigo"
+                    description="Cómo estar siempre conectado en cualquier campus de la universidad"
+                    href="/docs/activar-wifi"
+                >
+                    <ArrowRightIcon className="h-6 w-6 " />
+                </DocsInfoCard>
+            </div>
         </>
     );
 }
