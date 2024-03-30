2ade7fbb260bb163ec5f2ce0382561e27eb70048
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-03-30 08:03:30 +0100

    Add 'only dai members can sign in/up' warning in auth pages

--- src/app/auth/sign-up/[[...sign-up]]/page.tsx
+++ src/app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,17 @@
+import { Callout, CalloutDescription } from "@/components/ui/callout";
 import { AuthLayout } from "@/layouts/auth-layout";
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
     return (
         <AuthLayout>
-            <div className="mt-20">
+            <div className="flex w-full flex-col items-center justify-center">
+                <Callout variant="warning" className="sm:max-w-96 lg:max-w-96">
+                    <CalloutDescription>
+                        Por ahora solo miembros de la DAI con un correo
+                        electrónico @dai.uvigo.gal pueden crear una cuenta.
+                    </CalloutDescription>
+                </Callout>
                 <SignUp />
             </div>
         </AuthLayout>
