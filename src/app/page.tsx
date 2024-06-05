3a6f2041ecd6a39db3fd5cdfc7f3c83f259e1bb6
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-06-05 08:19:23 +0200

    Remove home page cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import Link from "next/link";
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card";
+
 import { DefaultLayout } from "@/layouts/default-layout";
-import { DocumentTextIcon, PencilIcon } from "@heroicons/react/24/outline";
 
 export default async function HomePage() {
     return (
@@ -21,34 +13,6 @@ export default async function HomePage() {
                     renueva para ofrecerte la mejor experiencia de la
                     Universidad de Vigo
                 </h2>
-                <div className="min-w-full space-y-5 lg:grid lg:grid-cols-3 lg:gap-4">
-                    <Card>
-                        <CardContent className="mt-5 flex items-center justify-center">
-                            <Link href="/docs">
-                                <DocumentTextIcon className="h-20 w-20" />
-                            </Link>
-                        </CardContent>
-                        <CardHeader>
-                            <CardTitle>Consejos y trucos</CardTitle>
-                            <CardDescription>
-                                Una guía para ayudarte en todo momento.
-                            </CardDescription>
-                        </CardHeader>
-                    </Card>
-                    <Card>
-                        <CardContent className="mt-5 flex items-center justify-center">
-                            <Link href="/blog">
-                                <PencilIcon className="h-20 w-20" />
-                            </Link>
-                        </CardContent>
-                        <CardHeader>
-                            <CardTitle>Un nuevo blog</CardTitle>
-                            <CardDescription>
-                                Un espacio para enterarte de todo lo importante.
-                            </CardDescription>
-                        </CardHeader>
-                    </Card>
-                </div>
             </div>
         </DefaultLayout>
     );
