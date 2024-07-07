aa249c00ef4697083ceb50a37125aa599fadc7f7
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-07-07 17:06:20 +0200

    Add user qr viewer page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { DashboardOverview } from "@/components/dashboard-overview/dashboard-overview";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function DashboardPage() {
     // const userInfo = useUserInfo();
@@ -9,11 +11,18 @@ export default function DashboardPage() {
         <div>
             <h1 className="mb-5 text-4xl font-bold">Panel de control</h1>
             {isAdmin && (
-                <div className="inline-flex items-center justify-center rounded-full bg-slate-900 px-5 py-1 text-slate-50">
+                <div className="inline-flex items-center justify-center rounded-full bg-slate-900 px-4 py-1 text-sm text-slate-50">
                     <p>Admin</p>
                 </div>
             )}
             <h2 className="my-5 text-3xl font-bold">Resumen</h2>
+            <Button asChild className="mb-5">
+                <Link
+                    href="/dashboard/qr"
+                >
+                    Ver mi QR
+                </Link>
+            </Button>
             <DashboardOverview
                 printingMaxValue={20}
                 printingCount={12}
