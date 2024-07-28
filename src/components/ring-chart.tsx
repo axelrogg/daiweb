1f8df54f3e90951a1b6d7a012e9f7436359ff3fe
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-07-28 08:57:39 +0200

    Remove unnecessary curly braces

--- src/components/ring-chart.tsx
+++ src/components/ring-chart.tsx
@@ -9,33 +9,31 @@ export const RingChart = ({
     currentValue,
     maxValue,
     className,
-}: RingChartProps) => {
-    return (
-        <div
-            className={cn(
-                "relative my-4 flex h-40 w-40 items-center justify-center",
-                className
-            )}
-        >
-            <ResponsiveRadialBar
-                colors={getColor(currentValue, maxValue)}
-                tracksColor="rgba(0, 0, 0, .08)"
-                data={data}
-                margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-                innerRadius={0.6}
-                padAngle={0.7}
-                cornerRadius={10}
-                maxValue={maxValue}
-                enableRadialGrid={false}
-                enableCircularGrid={false}
-                endAngle={360}
-                enableTracks={true}
-                radialAxisStart={null}
-                circularAxisOuter={null}
-            />
-        </div>
-    );
-};
+}: RingChartProps) => (
+    <div
+        className={cn(
+            "relative my-4 flex h-40 w-40 items-center justify-center",
+            className
+        )}
+    >
+        <ResponsiveRadialBar
+            colors={getColor(currentValue, maxValue)}
+            tracksColor="rgba(0, 0, 0, .08)"
+            data={data}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            innerRadius={0.6}
+            padAngle={0.7}
+            cornerRadius={10}
+            maxValue={maxValue}
+            enableRadialGrid={false}
+            enableCircularGrid={false}
+            endAngle={360}
+            enableTracks={true}
+            radialAxisStart={null}
+            circularAxisOuter={null}
+        />
+    </div>
+);
 
 interface RingChartProps {
     data: RingChartData[];
