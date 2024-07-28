ef2bc76a5efddde7e844f1899bb94809fddccdab
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-07-28 08:52:21 +0200

    Change admin status to staff

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,27 +1,27 @@
 import React from "react";
+import Link from "next/link";
 import { DashboardOverview } from "@/components/dashboard-overview/dashboard-overview";
 import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 export default function DashboardPage() {
     // const userInfo = useUserInfo();
-    const isAdmin = true;
+    const isStaff = true;
 
     return (
         <div>
             <h1 className="mb-5 text-4xl font-bold">Panel de control</h1>
-            {isAdmin && (
-                <div className="inline-flex items-center justify-center rounded-full bg-slate-900 px-4 py-1 text-sm text-slate-50">
-                    <p>Admin</p>
+            {isStaff && (
+                <div
+                    className="inline-flex items-center justify-center
+                                rounded-full bg-slate-900 px-4 py-1 text-sm
+                                text-slate-50"
+                >
+                    <p>Staff</p>
                 </div>
             )}
             <h2 className="my-5 text-3xl font-bold">Resumen</h2>
             <Button asChild className="mb-5">
-                <Link
-                    href="/dashboard/qr"
-                >
-                    Ver mi QR
-                </Link>
+                <Link href="/dashboard/mi-codigo-qr">Ver mi QR</Link>
             </Button>
             <DashboardOverview
                 printingMaxValue={20}
