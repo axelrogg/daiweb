f28dabe1b0510edee942f3acaa25763bfe8d376d
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-07-29 21:48:00 +0200

    add tabs to dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,33 +2,49 @@ import React from "react";
 import Link from "next/link";
 import { DashboardOverview } from "@/components/dashboard-overview/dashboard-overview";
 import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function DashboardPage() {
     // const userInfo = useUserInfo();
     const isStaff = true;
 
     return (
-        <div>
-            <h1 className="mb-5 text-4xl font-bold">Panel de control</h1>
+        <div className="flex flex-col items-start justify-center">
+            <h1 className="text-4xl font-bold">Dashboard</h1>
             {isStaff && (
                 <div
-                    className="inline-flex items-center justify-center
-                                rounded-full bg-slate-900 px-4 py-1 text-sm
-                                text-slate-50"
+                    className="mt-1 inline-flex items-center justify-center
+                                rounded-xl bg-gradient-to-r from-amber-400 to-amber-200 px-4
+                                py-1 text-sm text-slate-50"
                 >
-                    <p>Staff</p>
+                    <p className="text-black">Staff</p>
                 </div>
             )}
-            <h2 className="my-5 text-3xl font-bold">Resumen</h2>
-            <Button asChild className="mb-5">
+            <Button asChild className="mb-5 mt-5 w-full rounded-xl">
                 <Link href="/dashboard/mi-codigo-qr">Ver mi QR</Link>
             </Button>
-            <DashboardOverview
-                printingMaxValue={20}
-                printingCount={12}
-                borrowingMaxValue={5}
-                borrowingCount={2}
-            />
+            <div className="flex w-full items-center justify-center">
+                <Tabs defaultValue="resumen" className="w-full">
+                    <TabsList className="w-full">
+                        <TabsTrigger value="resumen" className="w-full">
+                            Resumen
+                        </TabsTrigger>
+                        <TabsTrigger value="staff-panel" className="w-full">
+                            Panel de staff
+                        </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="resumen" className="w-full">
+                        <DashboardOverview
+                            printingMaxValue={20}
+                            printingCount={12}
+                            borrowingMaxValue={5}
+                            borrowingCount={2}
+                        />
+                    </TabsContent>
+                    <TabsContent value="staff-panel">
+                    </TabsContent>
+                </Tabs>
+            </div>
         </div>
     );
 }
