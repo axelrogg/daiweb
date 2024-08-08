b5b9783937f1d7b3b66999fb3cabb42241b113b2
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-08 23:06:37 +0200

    remove justify-center class name on page title

--- src/app/dashboard/pedir-material/page.tsx
+++ src/app/dashboard/pedir-material/page.tsx
@@ -3,7 +3,7 @@ import { BorrowMaterialForm } from "@/components/dashboard/materials/borrow-mate
 export default function Page() {
     return (
         <div>
-            <h1 className="mb-5 flex justify-center text-2xl font-bold">
+            <h1 className="mb-5 flex text-3xl font-bold">
                 Préstamo de materiales
             </h1>
             <BorrowMaterialForm />
