2f2787b48b9535c571f1f59caf910ebeed682cae
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-08 23:10:25 +0200

    better table

--- src/components/dashboard/materials/active-loans-table.tsx
+++ src/components/dashboard/materials/active-loans-table.tsx
@@ -15,47 +15,52 @@ export const ActiveLoansTable = ({
     materialDetails,
 }: ActiveLoansTableProps) => (
     <Table>
-        <TableCaption>Lista de préstamos activos</TableCaption>
-        <TableHeader className="bg-gradient-to-r from-blue-500 to-cyan-500">
+        <TableHeader>
             <TableRow>
-                <TableHead className="w-[100px] rounded-tl-md font-bold text-white">
-                    Material
-                </TableHead>
-                <TableHead className="font-bold text-white">Inicio</TableHead>
-                <TableHead className="rounded-tr-md font-bold text-white">
-                    Fin
-                </TableHead>
+                <TableHead className="w-[70px]">Material</TableHead>
+                <TableHead>Inicio</TableHead>
+                <TableHead className="rounded-tr-md">Fin</TableHead>
             </TableRow>
         </TableHeader>
         <TableBody>
-            {materialDetails.map((details, idx) => {
-                const bgColor = idx % 2 == 0 ? "bg-gray-100" : "";
-                return (
-                    <TableRow key={idx} className={bgColor}>
-                        <TableCell className="text-sm">
-                            {details.material}
-                        </TableCell>
-                        <TableCell className="text-sm">
-                            {dateToDMY(details.createdAt)}
-                        </TableCell>
-                        <TableCell className="text-sm">
-                            {dateToDMY(details.validUntil)}
-                        </TableCell>
-                    </TableRow>
-                );
-            })}
+            {materialDetails?.length ? (
+                materialDetails.map((details, idx) => {
+                    const bgColor = idx % 2 == 0 ? "bg-gray-100" : "";
+                    return (
+                        <TableRow key={idx} className={bgColor}>
+                            <TableCell className="text-sm">
+                                {details.material}
+                            </TableCell>
+                            <TableCell className="text-sm">
+                                {dateToDMY(details.createdAt)}
+                            </TableCell>
+                            <TableCell className="text-sm">
+                                {dateToDMY(details.validUntil)}
+                            </TableCell>
+                        </TableRow>
+                    );
+                })
+            ) : (
+                <TableRow>
+                    <TableCell colSpan={3} className="h-24 text-center">
+                        No tienes ningún préstamo activo
+                    </TableCell>
+                </TableRow>
+            )}
         </TableBody>
     </Table>
 );
 
 interface ActiveLoansTableProps {
-    materialDetails: {
-        id: number;
-        userId: number;
-        isActive: boolean;
-        material: string;
-        createdAt: Date;
-        validUntil: Date;
-        status: string;
-    }[];
+    materialDetails:
+        | {
+              id: number;
+              userId: number;
+              isActive: boolean;
+              material: string;
+              createdAt: Date;
+              validUntil: Date;
+              status: string;
+          }[]
+        | null;
 }
