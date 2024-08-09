2a3ce72d3eeeaca0f35f7110a9ef7970307edef2
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-09 23:39:55 +0200

    finish this page

--- src/app/dashboard/mi-codigo-qr/page.tsx
+++ src/app/dashboard/mi-codigo-qr/page.tsx
@@ -1,59 +1,95 @@
-import Image from "next/image";
+import Link from "next/link";
 import QRCode from "react-qr-code";
-import { useUserInfo } from "@/lib/actions/useUserInfo";
+import { useUserInfo } from "@/lib/actions/user-info";
 
 export default async function Page() {
     const userInfo = await useUserInfo();
 
     if (!userInfo) {
         return (
-            <div className="">
-                <h1 className="mb-5 text-4xl font-bold">Mi código QR</h1>
-                <p>
-                    Parece que todavía no tienes un código QR asignado. Si crees
-                    que esto es un error, contacta con algun miembro de la DAI.
+            <div className="flex flex-col justify-center">
+                <h1 className="mb-4 flex justify-center text-3xl font-bold lg:mb-24">
+                    Mi código QR
+                </h1>
+                <p className="text-center">
+                    Si lees esto es porque estamos experimentando algunos
+                    problemas por el momento.
+                </p>
+                <br />
+                <p className="text-center">
+                    Si luego de actualizar esta página sigues viendo este
+                    mensage, por favor contacta con algún miembro de la DAI.
+                </p>
+                <br />
+                <p className="text-center">
+                    Sentimos cualquier inconveniente que este problema pueda
+                    causar.
                 </p>
             </div>
         );
     }
 
+    const userIdsConcat = userInfo.id.toString() + "." + userInfo.externalId;
+
     return (
-        <div className="align-center flex flex-col justify-center">
-            <h1 className="mb-24 text-4xl font-bold">Mi código QR</h1>
-            <div
-                className="absolute top-40 h-28 w-28 self-center rounded-full
-                            drop-shadow-md"
-            >
-                <Image
-                    priority
-                    src={userInfo.profilePicUrl}
-                    alt="Foto de perfil"
-                    fill={true}
-                    className="rounded-full"
-                />
-            </div>
-            <div
-                className="flex h-[25rem] flex-col items-center justify-end
-                           space-y-6 rounded-lg bg-gradient-to-b from-blue-500
-                           to-cyan-500 pb-10"
-            >
-                <h2 className="absolute top-72 text-2xl font-bold text-white">
-                    {userInfo.fullName}
-                </h2>
-                <div
-                    className="align-center flex h-60 w-60 justify-center
-                                rounded-lg bg-white"
-                >
-                    <QRCode
-                        size={200}
-                        style={{ height: "auto" }}
-                        viewBox={`0 0 256 256`}
-                        level="H"
-                        value={Buffer.from(userInfo.id.toString()).toString(
-                            "base64"
-                        )}
-                    />
+        <div className="flex flex-col justify-center">
+            <h1 className="mb-4 flex justify-center text-3xl font-bold lg:mb-24">
+                Mi código QR
+            </h1>
+            {userInfo.isVerified && (
+                <div className="flex justify-center">
+                    <p className="mb-5 text-center lg:w-2/5">
+                        Muestra este código en cualquier sede de la DAI para
+                        acceder a préstamos de materiales.
+                    </p>
                 </div>
+            )}
+            <div
+                className="flex h-[25rem] flex-col items-center justify-center
+                           rounded-lg bg-gradient-to-b from-blue-500
+                           to-cyan-500 pb-10 drop-shadow-2xl lg:w-2/5 lg:self-center"
+            >
+                {userInfo.isVerified ? (
+                    <div
+                        className="flex h-60 w-60 items-center justify-center
+                                rounded-lg bg-white drop-shadow-2xl"
+                    >
+                        <QRCode
+                            size={200}
+                            style={{ height: "auto" }}
+                            viewBox={`0 0 1000 1000`}
+                            level="H"
+                            value={Buffer.from(userIdsConcat).toString(
+                                "base64"
+                            )}
+                        />
+                    </div>
+                ) : (
+                    <div
+                        className="align-center flex flex h-60 w-3/5 flex-col justify-center
+                                rounded-lg"
+                    >
+                        <p className="mb-3 text-center text-xl font-bold text-white">
+                            Todavía no tienes un QR
+                        </p>
+                        <p className="text-center text-white">
+                            Para obtener tu propio QR y poder acceder a todos
+                            los servicios de la DAI,{" "}
+                            <Link
+                                href="/guias/como-verificar-mi-cuenta"
+                                className="underline"
+                            >
+                                verifica tu cuenta
+                            </Link>
+                            .
+                        </p>
+                        <br />
+                        <p className="text-center text-white">
+                            Si crees que esto es un error, contacta con algun
+                            miembro de la DAI.
+                        </p>
+                    </div>
+                )}
             </div>
         </div>
     );
