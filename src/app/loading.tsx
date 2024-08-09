6a4ddf189e787c85d98372c411540ec99a664914
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-09 23:39:29 +0200

    add loading spinner to loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,10 @@
+import { LoadingSpinner } from "@/components/loading-spinner";
+
 export default function LoadingPage() {
-    return <p>Cargando...</p>;
+    return (
+        <div className="flex flex-col items-center justify-center lg:mt-32">
+            <p className="mb-10 text-2xl font-bold">Cargando</p>
+            <LoadingSpinner className="text-primary" />
+        </div>
+    );
 }
