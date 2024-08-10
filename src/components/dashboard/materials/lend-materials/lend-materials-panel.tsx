90ad12d10800b178f142c4a381743ce17b0f105a
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-10 20:31:38 +0200

    use new server action name userInfoFromId

--- src/components/dashboard/materials/lend-materials/lend-materials-panel.tsx
+++ src/components/dashboard/materials/lend-materials/lend-materials-panel.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { MulticodeScanner } from "@/components/multicode-scanner";
 import { Button } from "@/components/ui/button";
 import { extUserActiveReservations } from "@/lib/actions/user-active-reservations";
-import { useUserInfoFromId } from "@/lib/actions/user-info-from-id";
+import { userInfoFromId } from "@/lib/actions/user-info-from-id";
 import { MaterialReservations } from "@/types/actions";
 import { DataTable } from "./lend-materials-data-table";
 import { columns } from "./lend-materials-data-column";
@@ -16,29 +16,29 @@ export const LendMaterialsPanel = () => {
     const [materialReservations, setMaterialReservations] =
         useState<MaterialReservations>([]);
 
-    function decodeUserId(code: string | null) {
-        if (!code) return null;
-        const userId = Number(atob(code));
-        if (Number.isNaN(userId)) return null;
-        return userId;
-    }
-
     useEffect(() => {
-        async function activeReservations() {
+        const activeReservations = async () => {
             const userId = decodeUserId(code);
             if (!userId) {
                 return;
             }
             setDataIsLoading(true);
             const reservations = await extUserActiveReservations(userId);
-            const userInfo = await useUserInfoFromId(userId);
+            const userInfo = await userInfoFromId(userId);
             setMaterialReservations((old) => [...old, ...reservations]);
             setFullName(userInfo!.fullName);
             setDataIsLoading(false);
-        }
+        };
 
         activeReservations();
     }, [code]);
+
+    function decodeUserId(code: string | null) {
+        if (!code) return null;
+        const userId = Number(atob(code));
+        if (Number.isNaN(userId)) return null;
+        return userId;
+    }
 
     return (
         <>
