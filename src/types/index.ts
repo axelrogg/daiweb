d8bd50b2447bec725152d26c1af4daee9a61e53d
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-10 20:32:18 +0200

    remove export from deleted file @/types/database.types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,5 +2,4 @@ export * from "@/types/props-with-children.types";
 export * from "@/types/http-response-status.types";
 export * from "@/types/clerk-webhook.types";
 export * from "@/types/oauth.types";
-export * from "@/types/database.types";
 export * from "@/types/environment.types";
