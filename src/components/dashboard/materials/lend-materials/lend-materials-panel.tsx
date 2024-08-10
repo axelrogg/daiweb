ea6f44dd78555656ab0ee3880fdee8fc42c3b770
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-10 13:51:35 +0200

    refactor

--- src/components/dashboard/materials/lend-materials/lend-materials-panel.tsx
+++ src/components/dashboard/materials/lend-materials/lend-materials-panel.tsx
@@ -3,67 +3,58 @@
 import { useEffect, useState } from "react";
 import { MulticodeScanner } from "@/components/multicode-scanner";
 import { Button } from "@/components/ui/button";
-import { extUserActiveReservations } from "@/lib/actions/userActiveReservations";
-import { getUserFullNameFromUserId } from "@/lib/actions/getUserFullNameFromUserId";
+import { extUserActiveReservations } from "@/lib/actions/user-active-reservations";
+import { useUserInfoFromId } from "@/lib/actions/user-info-from-id";
 import { MaterialReservations } from "@/types/actions";
 import { DataTable } from "./lend-materials-data-table";
 import { columns } from "./lend-materials-data-column";
 
 export const LendMaterialsPanel = () => {
+    const [dataIsLoading, setDataIsLoading] = useState(false);
     const [code, setCode] = useState<string | null>(null);
-    const [userFullName, setUserFullName] = useState<string | null>(null);
-    const [material, setMaterial] = useState<string | null>(null);
-    const [showTable, setShowTable] = useState(false);
+    const [fullName, setFullName] = useState<string | null>(null);
     const [materialReservations, setMaterialReservations] =
         useState<MaterialReservations>([]);
 
+    function decodeUserId(code: string | null) {
+        if (!code) return null;
+        const userId = Number(atob(code));
+        if (Number.isNaN(userId)) return null;
+        return userId;
+    }
+
     useEffect(() => {
-        async function hello() {
-            const userId = Number(atob(code!));
+        async function activeReservations() {
+            const userId = decodeUserId(code);
+            if (!userId) {
+                return;
+            }
+            setDataIsLoading(true);
             const reservations = await extUserActiveReservations(userId);
-
-            setMaterialReservations((old) => [...old!, ...reservations!]);
-            setShowTable(true);
+            const userInfo = await useUserInfoFromId(userId);
+            setMaterialReservations((old) => [...old, ...reservations]);
+            setFullName(userInfo!.fullName);
+            setDataIsLoading(false);
         }
 
-        if (typeof code === "string") {
-            hello();
-        }
-        console.log(materialReservations);
-    }, [code]);
-
-    useEffect(() => {
-        async function getUserFullName() {
-            const userId = Number(atob(code!));
-            const fullName = await getUserFullNameFromUserId(userId);
-            setUserFullName(fullName!);
-        }
-
-        if (typeof code === "string") {
-            getUserFullName();
-        }
+        activeReservations();
     }, [code]);
 
     return (
         <>
-            {!showTable && (
-                <div className="mb-5 flex items-center justify-center">
-                    <p className="text-center text-sm text-gray-500">
-                        Para empezar, escanea el QR de un alumno
+            <div>
+                {fullName && (
+                    <p className="mb-3">
+                        Nombre y apellidos:{" "}
+                        <span className="font-bold">{fullName}</span>
                     </p>
-                </div>
-            )}
-            {
-                <div>
-                    {userFullName && (
-                        <p className="mb-3">
-                            Alumno/a:{" "}
-                            <span className="font-bold">{userFullName}</span>
-                        </p>
-                    )}
-                    <DataTable columns={columns} data={materialReservations} />
-                </div>
-            }
+                )}
+                <DataTable
+                    columns={columns}
+                    data={materialReservations}
+                    loading={dataIsLoading}
+                />
+            </div>
             <MulticodeScanner setScannedCode={setCode}>
                 <Button className="w-full">Escanear código QR</Button>
             </MulticodeScanner>
