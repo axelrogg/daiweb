05cf5185cec4923cf5feeb89e3ca1e7aacf5517f
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-10 13:45:13 +0200

    remove PopoverPortal

--- src/app/dashboard/mis-prestamos/page.tsx
+++ src/app/dashboard/mis-prestamos/page.tsx
@@ -1,11 +1,10 @@
 import { ActiveLoansTable } from "@/components/dashboard/materials/active-loans-table";
-import { Popover } from "@/components/ui/popover";
 import { userActiveLoans } from "@/lib/actions/user-active-loans";
 import {
+    Popover,
     PopoverContent,
-    PopoverPortal,
     PopoverTrigger,
-} from "@radix-ui/react-popover";
+} from "@/components/ui/popover";
 import { CircleHelpIcon } from "lucide-react";
 
 export default async function Page() {
@@ -20,27 +19,25 @@ export default async function Page() {
                         <CircleHelpIcon className="h-6 w-6 text-primary" />
                     </PopoverTrigger>
                 </div>
-                <PopoverPortal>
-                    <PopoverContent
-                        className="z-50 w-64 rounded-lg bg-white p-6 drop-shadow-2xl lg:w-3/5"
-                        sideOffset={10}
-                        align="start"
-                    >
-                        <div className="flex flex-col space-y-2">
-                            <p className="mb-3 text-xl font-bold">Tip</p>
-                            <p>Aquí podrás revisar tus préstamos activos.</p>
-                            <p>
-                                Recuerda que, con la excepción de los
-                                portátiles, solo puedes tener un material por 5
-                                días consecutivos.
-                            </p>
-                            <p>
-                                Para devolver un material puedes acercarte a
-                                cualquier sede de la DAI.
-                            </p>
-                        </div>
-                    </PopoverContent>
-                </PopoverPortal>
+                <PopoverContent
+                    className="z-50 w-64 rounded-lg bg-white p-6 drop-shadow-2xl lg:w-3/5"
+                    sideOffset={10}
+                    align="start"
+                >
+                    <div className="flex flex-col space-y-2">
+                        <p className="mb-3 text-xl font-bold">Tip</p>
+                        <p>Aquí podrás revisar tus préstamos activos.</p>
+                        <p>
+                            Recuerda que, con la excepción de los portátiles,
+                            solo puedes tener un material por 5 días
+                            consecutivos.
+                        </p>
+                        <p>
+                            Para devolver un material puedes acercarte a
+                            cualquier sede de la DAI.
+                        </p>
+                    </div>
+                </PopoverContent>
             </Popover>
             <ActiveLoansTable materialDetails={activeLoans} />
         </>
