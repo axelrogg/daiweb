87e2da727929f468ac133c3064920b4c59ec27cd
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-13 13:24:20 +0200

    add conditional renders depending on whether the user's verification and staff status

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,164 +1,141 @@
 import React from "react";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { DashboardCard } from "@/components/dashboard/cards/dashboard-card";
-import { LendMaterialsPanel } from "@/components/dashboard/materials/lend-materials/lend-materials-panel";
 import { MaterialLoansOverviewPanel } from "@/components/dashboard/materials/materials-loans-overview-panel";
-import { useUserInfo } from "@/lib/actions/user-info";
+import { userInfo } from "@/lib/actions/user-info";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { CircleHelpIcon } from "lucide-react";
+import { BadgeCheckIcon, CircleHelpIcon } from "lucide-react";
 import {
     Popover,
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
+import { NoUserInfo } from "@/components/dashboard/materials/no-user-info";
+import { Callout, CalloutDescription } from "@/components/ui/callout";
 
 export default async function DashboardPage() {
-    const userInfo = await useUserInfo();
-    if (!userInfo) {
-        return null;
+    const user = await userInfo();
+    if (!user) {
+        return <NoUserInfo />;
+    }
+
+    if (!user.isVerified) {
+        return (
+            <div className="flex h-[80svh] w-full flex-col items-start items-center">
+                <h1 className="self-start text-4xl font-bold">Dashboard</h1>
+                <Callout variant="warning" className="max-w-lg">
+                    <CalloutDescription>
+                        Parece que no has verificado tu cuenta todavía. Para
+                        poder acceder a todos los beneficios de la DAI,
+                        necesitas{" "}
+                        <Link
+                            className="font-bold underline"
+                            href="/verificar-cuenta"
+                        >
+                            verificar tu cuenta
+                        </Link>
+                        .
+                    </CalloutDescription>
+                </Callout>
+            </div>
+        );
     }
 
     return (
         <div className="flex flex-col items-start justify-center">
             <h1 className="text-4xl font-bold">Dashboard</h1>
-            {userInfo.isStaff && (
-                <div
-                    className="line-flex mt-1 items-center justify-center
-                                rounded-xl bg-gradient-to-r from-amber-400 to-amber-200 px-4
-                                py-1 text-sm text-slate-50"
+            <div className="group flex h-10 flex-row items-center space-x-3">
+                {user.isVerified && (
+                    <div>
+                        <BadgeCheckIcon className="text-primary" />
+                    </div>
+                )}
+                {user.isStaff && (
+                    <div
+                        className="line-flex items-center justify-center
+                                    rounded-xl bg-gradient-to-r from-amber-400 to-amber-200 px-4
+                                    py-1 text-sm text-slate-50"
+                    >
+                        <p className="text-black">Staff</p>
+                    </div>
+                )}
+            </div>
+            <div className="group my-5 w-full space-y-5 lg:space-x-5">
+                {user.isStaff && (
+                    <Button
+                        asChild
+                        className="w-full rounded-xl bg-gradient-to-r from-amber-400 to-amber-200 text-black lg:max-w-40 lg:rounded-lg"
+                    >
+                        <Link href="/dashboard/staff-panel">
+                            Ir al panel de staff
+                        </Link>
+                    </Button>
+                )}
+                <Button
+                    asChild
+                    className="w-full rounded-xl lg:max-w-24 lg:rounded-lg"
                 >
-                    <p className="text-black">Staff</p>
+                    <Link href="/dashboard/mi-codigo-qr">Ver mi QR</Link>
+                </Button>
+            </div>
+            <div className="flex w-full flex-col items-center justify-center">
+                <div className="group w-full space-y-4">
+                    <Card className="flex flex-col justify-between">
+                        <CardHeader className="flex items-start pb-3">
+                            <Popover>
+                                <div className="mb-5 flex flex-row">
+                                    <CardTitle className="mr-3">
+                                        Mis préstamos
+                                    </CardTitle>
+                                    <PopoverTrigger>
+                                        <CircleHelpIcon className="h-6 w-6 text-primary" />
+                                    </PopoverTrigger>
+                                </div>
+                                <PopoverContent
+                                    className="z-50 w-64 rounded-lg bg-white p-6 drop-shadow-2xl lg:w-3/5"
+                                    sideOffset={10}
+                                    align="start"
+                                    collisionPadding={20}
+                                >
+                                    <div className="flex flex-col space-y-2">
+                                        <p className="mb-3 text-xl font-bold text-primary">
+                                            Tip
+                                        </p>
+                                        <p>
+                                            En este panel los préstamos activos
+                                            a tu nombre.
+                                        </p>
+                                        <p>
+                                            Si quieres pedir algún material, haz
+                                            click en el botón{" "}
+                                            <span className="font-bold">
+                                                Pedir materiales
+                                            </span>
+                                            .
+                                        </p>
+                                        <p>
+                                            Recuerda que solo puedes tener 5
+                                            préstamos activos en un mismo
+                                            momento.
+                                        </p>
+                                        <p>
+                                            Si quieres ver los detalles de los
+                                            materiales que tienes, haz click en
+                                            el botón{" "}
+                                            <span className="font-bold">
+                                                Ver detalles
+                                            </span>
+                                            .
+                                        </p>
+                                    </div>
+                                </PopoverContent>
+                            </Popover>
+                        </CardHeader>
+                        <CardContent className="pt-3">
+                            <MaterialLoansOverviewPanel />
+                        </CardContent>
+                    </Card>
                 </div>
-            )}
-            <Button
-                asChild
-                className="mb-5 mt-5 w-full rounded-xl lg:max-w-24 lg:rounded-lg"
-            >
-                <Link href="/dashboard/mi-codigo-qr">Ver mi QR</Link>
-            </Button>
-            <div className="flex w-full items-center justify-center">
-                <Tabs defaultValue="resumen" className="w-full">
-                    <TabsList className="w-full">
-                        <TabsTrigger value="resumen" className="w-full">
-                            Resumen
-                        </TabsTrigger>
-                        <TabsTrigger value="staff-panel" className="w-full">
-                            Panel de staff
-                        </TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="resumen" className="w-full">
-                        <div className="group w-full">
-                            <Card className="flex flex-col justify-between">
-                                <CardHeader className="flex items-start pb-3">
-                                    <Popover>
-                                        <div className="mb-5 flex flex-row">
-                                            <CardTitle className="mr-3">
-                                                Mis préstamos
-                                            </CardTitle>
-                                            <PopoverTrigger>
-                                                <CircleHelpIcon className="h-6 w-6 text-primary" />
-                                            </PopoverTrigger>
-                                        </div>
-                                        <PopoverContent
-                                            className="z-50 w-64 rounded-lg bg-white p-6 drop-shadow-2xl lg:w-3/5"
-                                            sideOffset={10}
-                                            align="start"
-                                            collisionPadding={20}
-                                        >
-                                            <div className="flex flex-col space-y-2">
-                                                <p className="mb-3 text-xl font-bold text-primary">
-                                                    Tip
-                                                </p>
-                                                <p>
-                                                    En este panel los préstamos
-                                                    activos a tu nombre.
-                                                </p>
-                                                <p>
-                                                    Si quieres pedir algún
-                                                    material, haz click en el
-                                                    botón{" "}
-                                                    <span className="font-bold">
-                                                        Pedir materiales
-                                                    </span>
-                                                    .
-                                                </p>
-                                                <p>
-                                                    Recuerda que solo puedes
-                                                    tener 5 préstamos activos en
-                                                    un mismo momento.
-                                                </p>
-                                                <p>
-                                                    Si quieres ver los detalles
-                                                    de los materiales que
-                                                    tienes, haz click en el
-                                                    botón{" "}
-                                                    <span className="font-bold">
-                                                        Ver detalles
-                                                    </span>
-                                                    .
-                                                </p>
-                                            </div>
-                                        </PopoverContent>
-                                    </Popover>
-                                </CardHeader>
-                                <CardContent className="pt-3">
-                                    <MaterialLoansOverviewPanel />
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </TabsContent>
-                    <TabsContent value="staff-panel">
-                        <div className="group w-full">
-                            <Card className="flex flex-col justify-between">
-                                <CardHeader className="flex items-start pb-3">
-                                    <Popover>
-                                        <div className="mb-5 flex flex-row">
-                                            <CardTitle className="mr-3">
-                                                Prestar materiales
-                                            </CardTitle>
-                                            <PopoverTrigger>
-                                                <CircleHelpIcon className="h-6 w-6 text-primary" />
-                                            </PopoverTrigger>
-                                        </div>
-                                        <PopoverContent
-                                            className="z-50 w-64 rounded-lg bg-white p-6 drop-shadow-2xl lg:w-3/5"
-                                            sideOffset={10}
-                                            align="start"
-                                            collisionPadding={20}
-                                        >
-                                            <div className="flex flex-col space-y-2">
-                                                <p className="mb-3 text-xl font-bold text-primary">
-                                                    Tip
-                                                </p>
-                                                <p>
-                                                    En este panel podrás prestar
-                                                    materiales a cualquier
-                                                    estudiante.
-                                                </p>
-                                                <p>
-                                                    Si no tienes claro qué hacer
-                                                    revisa la{" "}
-                                                    <Link
-                                                        href="/guias/staff/como-realizar-prestamos"
-                                                        className="hover:underline"
-                                                    >
-                                                        guía sobre préstamos
-                                                    </Link>
-                                                    .
-                                                </p>
-                                            </div>
-                                        </PopoverContent>
-                                    </Popover>
-                                </CardHeader>
-                                <CardContent className="pt-3">
-                                    <LendMaterialsPanel />
-                                </CardContent>
-                            </Card>
-                        </div>
-                    </TabsContent>
-                </Tabs>
             </div>
         </div>
     );
