41faaa96c12686365554585b5a5b51fc3c959874
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-13 13:14:56 +0200

    revamp loading page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,10 +1,20 @@
 import { LoadingSpinner } from "@/components/loading-spinner";
+import { MainLogo } from "@/components/main-logo";
+import Image from "next/image";
 
 export default function LoadingPage() {
     return (
-        <div className="flex flex-col items-center justify-center lg:mt-32">
-            <p className="mb-10 text-2xl font-bold">Cargando</p>
-            <LoadingSpinner className="text-primary" />
+        <div className="group flex h-[80vh] flex-col items-center justify-center space-y-4">
+            <div className="animate-pulse ">
+                <Image
+                    src="/logo-dai.svg"
+                    alt="DAI logo"
+                    width={80}
+                    height={62}
+                    priority
+                />
+            </div>
+            <p className="text-center text-xl font-bold">Cargando...</p>
         </div>
     );
 }
