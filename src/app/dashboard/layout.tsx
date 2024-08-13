436ee0f43985c2ad32981713827f6649fd30d417
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-13 13:14:11 +0200

    use DefaultLayout for the dashboard/ pages

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,9 @@
-import { MainLogo } from "@/components/main-logo";
+import { DefaultLayout } from "@/layouts/default-layout";
 
 export default function DashboardLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
-    return (
-        <div className="mx-5 flex min-h-dvh flex-col lg:mx-80">
-            <nav className="mb-2 py-5">
-                <MainLogo />
-            </nav>
-            {children}
-        </div>
-    );
+    return <DefaultLayout>{children}</DefaultLayout>;
 }
