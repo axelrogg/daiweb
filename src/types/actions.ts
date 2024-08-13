831d9fa29e283d6c21d27453609fc006f3ab90f5
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-13 13:45:46 +0200

    add MaterialLoan action type

--- src/types/actions.ts
+++ src/types/actions.ts
@@ -10,6 +10,22 @@ export type MaterialReservation = {
 
 export type MaterialReservations = MaterialReservation[];
 
+export type MaterialLoan = {
+    id: number;
+    material: string;
+    isActive: boolean;
+    createdAt: Date;
+    validUntil: Date;
+    //renewed_count: number
+    userId: number;
+    //loanedBy: number;
+    //lastUpdatedAt: Date
+    status: string;
+    //returnedBy: number
+};
+
+export type MaterialLoans = MaterialLoan[];
+
 export type DbUserInfo = {
     id: number;
     external_id: string;
