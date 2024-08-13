391a88050589ded992d8039f2649f2c269c89d98
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-13 20:27:11 +0200

    protect /auth routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isDashboardRoute = createRouteMatcher(["/dashboard(.*)"]);
+const isAuthRoute = createRouteMatcher(["/auth(.*)"])
 
 export default clerkMiddleware((auth, req) => {
     // Restrict dashboard routes to logged in users
-    if (isDashboardRoute(req)) {
+    if (isDashboardRoute(req) || isAuthRoute(req)) {
         auth().protect();
     }
 });
