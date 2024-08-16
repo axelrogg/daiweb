c3b23ec85e6de2de2fafb3fe1fb3cb541346fd8a
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-16 10:56:55 +0200

    add google sign in button

--- src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,13 @@
 import { Callout, CalloutDescription } from "@/components/ui/callout";
 import { AuthLayout } from "@/components/layouts/auth-layout";
 import { signIn } from "@/auth";
+import Image from "next/image";
 
 export default function SignInPage() {
     return (
         <AuthLayout>
             <div className="flex w-full flex-col items-center justify-center">
-                <Callout variant="warning" className="sm:max-w-96 lg:max-w-96">
+                <Callout variant="warning" className="sm:max-w-96 lg:max-w-96 mb-5">
                     <CalloutDescription>
                         Por ahora solo miembros de la DAI con un correo
                         electrónico @dai.uvigo.gal pueden acceder al Dashboard.
@@ -15,15 +16,28 @@ export default function SignInPage() {
                         DAI o acércate a una de sus sedes.
                     </CalloutDescription>
                 </Callout>
-            </div>
             <form
                 action={async () => {
                     "use server";
                     await signIn("google", { redirectTo: "/dashboard" });
                 }}
             >
-                <button type="submit">Signin with Google</button>
+                <button
+                    type="submit"
+                    className="p-4 flex flew-row items-center justify-center
+                               border border-neutral-400 rounded-lg"
+                >
+                    <Image
+                        src="/assets/google-g-logo.svg"
+                        alt="Google logo"
+                        width={20}
+                        height={20}
+                        className="mr-2"
+                    />
+                    Iniciar sesión con Google
+                </button>
             </form>
+            </div>
         </AuthLayout>
     );
 }
