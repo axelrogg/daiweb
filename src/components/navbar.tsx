53eb8ae4db6ebd0dd6ea5509dcbd76402b6f90ee
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-16 23:27:03 +0200

    add navbar user button

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { ReactElement, useEffect, useState } from "react";
+import { ReactElement } from "react";
 import Link from "next/link";
 import { DocumentTextIcon } from "@heroicons/react/24/outline";
 import { Menu } from "lucide-react";
@@ -21,9 +19,7 @@ import {
 } from "./ui/sheet";
 import { MainLogo } from "@/components/main-logo";
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import { UserInfo } from "@/types/actions";
-import { userInfo } from "@/lib/actions/user/user-info";
+import { UserButton } from "./auth/user-button";
 
 const navBarSheetItems: NavBarSheetItem[] = [
     {
@@ -81,71 +77,52 @@ function renderNavBarSheetItem(item?: NavBarSheetItem | null) {
 }
 
 export const NavBar = () => {
-    const [user, setUser] = useState<UserInfo | null>(null)
-
-    useEffect(() => {
-        async function getUserInfo() {
-            const info = await userInfo()
-            setUser(info)
-        }
-        getUserInfo()
-
-    }, [])
     return (
-    <nav className="mb-2 flex items-center justify-center py-5 lg:justify-between">
-        <MainLogo />
-        <NavigationMenu className="hidden lg:flex">
-            <NavigationMenuList>
-                <NavigationMenuItem>
-                    <Link href="/guias" legacyBehavior passHref>
-                        <NavigationMenuLink
-                            className={navigationMenuTriggerStyle()}
-                        >
-                            <DocumentTextIcon className="mr-2 h-6 w-6 stroke-purple-800" />
-                            Guías
-                        </NavigationMenuLink>
-                    </Link>
-                </NavigationMenuItem>
-            </NavigationMenuList>
-        </NavigationMenu>
-        <div className="hidden lg:flex">
-            <Button asChild>
-                <Link href="/dashboard">Dashboard</Link>
-            </Button>
-        </div>
-        <Sheet>
-            <SheetTitle className="hidden">Menu</SheetTitle>
-            <SheetDescription className="hidden">Menu</SheetDescription>
-            <SheetTrigger className="absolute left-5 top-6 lg:hidden">
-                <Menu className="h-6 w-6" />
-                <span className="sr-only">Open Menu</span>
-            </SheetTrigger>
-            <SheetContent side="left">
-                <MainLogo />
-                <div className="my-6">
-                    {navBarSheetItems.map((item) =>
-                        renderNavBarSheetItem(item)
-                    )}
-                </div>
-                <SheetClose>
-                    <Button asChild className="w-full">
-                        <Link href="/dashboard">Dashboard</Link>
-                    </Button>
-                </SheetClose>
-            </SheetContent>
-        </Sheet>
-        {user && (
-            <div className="absolute right-5 top-5 lg:hidden">
-                <Image
-                    src={user.pictureUri}
-                    className="rounded-full"
-                    alt="Foto de perfil. Haz click para acceder a tu perfil"
-                    width={35}
-                    height={35}
-                    priority
-                />
+        <nav className="mb-2 flex items-center justify-center py-5 lg:justify-between">
+            <MainLogo />
+            <NavigationMenu className="hidden lg:flex">
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Link href="/guias" legacyBehavior passHref>
+                            <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                            >
+                                <DocumentTextIcon className="mr-2 h-6 w-6 stroke-purple-800" />
+                                Guías
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            <div className="hidden lg:flex">
+                <Button asChild>
+                    <Link href="/dashboard">Dashboard</Link>
+                </Button>
             </div>
-        )}
-    </nav>
-);
-}
+            <Sheet>
+                <SheetTitle className="hidden">Menu</SheetTitle>
+                <SheetDescription className="hidden">Menu</SheetDescription>
+                <SheetTrigger className="absolute left-5 top-6 lg:hidden">
+                    <Menu className="h-6 w-6" />
+                    <span className="sr-only">Open Menu</span>
+                </SheetTrigger>
+                <SheetContent side="left">
+                    <MainLogo />
+                    <div className="my-6">
+                        {navBarSheetItems.map((item) =>
+                            renderNavBarSheetItem(item)
+                        )}
+                    </div>
+                    <SheetClose>
+                        <Button asChild className="w-full">
+                            <Link href="/dashboard">Dashboard</Link>
+                        </Button>
+                    </SheetClose>
+                </SheetContent>
+            </Sheet>
+            <div className="absolute right-5 top-5 lg:hidden">
+                <UserButton />
+            </div>
+        </nav>
+    );
+};
