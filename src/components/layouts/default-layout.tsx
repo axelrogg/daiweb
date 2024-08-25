eca07b34faf5f456969045ad25ceaa7894446c54
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-08-25 13:01:15 +0200

    wrap navbar and children with flexible boxes

--- src/components/layouts/default-layout.tsx
+++ src/components/layouts/default-layout.tsx
@@ -2,9 +2,13 @@ import { NavBar } from "@/components/navbar";
 import { WithFooter } from "@/components/with-footer";
 
 export const DefaultLayout = ({ children }: { children: React.ReactNode }) => (
-    <div className="mx-5 flex min-h-dvh flex-col lg:mx-20 2xl:mx-80">
-        <NavBar />
-        {children}
+    <div className="flex min-h-[80svh] flex-col">
+        <div className="lg:mx-20 2xl:mx-80">
+            <NavBar />
+        </div>
+        <div className="mx-5 mb-10 flex min-h-[100svh] flex-col lg:mx-20 2xl:mx-80">
+            {children}
+        </div>
         <WithFooter />
     </div>
 );
