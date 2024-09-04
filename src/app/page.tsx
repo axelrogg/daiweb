6c603b08e2af764e42cd9fe0b5a4daa696fef528
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-09-04 18:01:32 +0200

    prettify tailwind class order

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default async function HomePage() {
                         Industriales: tu guía en la Universidad de Vigo
                     </h2>
                 </div>
-                <div className="flex flex-col lg:flex-row h-[90svh] items-center justify-center space-y-3 px-4">
+                <div className="flex h-[90svh] flex-col items-center justify-center space-y-3 px-4 lg:flex-row">
                     <LayoutDashboardIcon className="h-2/5 w-2/5 text-primary" />
                     <div className="flex flex-col items-center justify-center space-y-4">
                         <h1 className="text-center text-5xl font-bold">
@@ -31,7 +31,7 @@ export default async function HomePage() {
                         </h2>
                     </div>
                 </div>
-                <div className="flex flex-col lg:flex-row h-[90svh] flex-row items-center justify-center space-y-3 px-4">
+                <div className="flex h-[90svh] flex-row flex-col items-center justify-center space-y-3 px-4 lg:flex-row">
                     <DocumentTextIcon className="h-2/5 w-2/5 stroke-purple-800 lg:hidden" />
                     <div className="flex flex-col items-center justify-center space-y-4">
                         <h1 className="text-center text-5xl font-bold">
@@ -42,7 +42,7 @@ export default async function HomePage() {
                             navegar tu vida universitaria
                         </h2>
                     </div>
-                    <DocumentTextIcon className="h-2/5 w-2/5 stroke-purple-800 hidden lg:flex" />
+                    <DocumentTextIcon className="hidden h-2/5 w-2/5 stroke-purple-800 lg:flex" />
                 </div>
             </div>
             <Footer />
