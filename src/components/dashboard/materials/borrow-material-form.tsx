7561f65441264ecc773c3430750cbf82efaa4227
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-10-18 18:08:02 +0200

    Remove justify-center className from form

--- src/components/dashboard/materials/borrow-material-form.tsx
+++ src/components/dashboard/materials/borrow-material-form.tsx
@@ -84,7 +84,7 @@ export const BorrowMaterialForm = () => {
     }
 
     return (
-        <div className="flex justify-center">
+        <div className="flex">
             <Form {...form}>
                 <form
                     onSubmit={form.handleSubmit(onSubmit)}
