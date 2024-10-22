f03e42446878990da4e25cde91083f2f6c473a41
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-10-22 07:25:34 +0200

    request user info in dashboard layour

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,16 +1,23 @@
+import { NoUserInfo } from "@/components/dashboard/materials/no-user-info";
 import { DashboardContentBar } from "@/components/layouts/complements/dashboard-content-bar";
 import { DashboardLayout } from "@/components/layouts/dashboard-layout";
+import { userInfo } from "@/lib/actions/user/user-info";
 
 export default async function Layout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const user = await userInfo();
+    if (!user) {
+        return <NoUserInfo />;
+    }
+
     return (
         <DashboardLayout>
             <div className="flex lg:flex-row">
                 <div className="hidden flex-col overflow-y-auto lg:flex lg:w-1/4">
-                    <DashboardContentBar />
+                    <DashboardContentBar isStaff={user.isStaff}/>
                 </div>
                 <div className="min-h-[80svh] flex-1 lg:ml-10">{children}</div>
             </div>
