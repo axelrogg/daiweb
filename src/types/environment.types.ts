44be37979d3c7d9eb6a2016672c713db63ac6beb
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-11-02 16:29:01 +0100

    Add EMAIL_SERVICE_SECRET environment variable to authorize email sending

--- src/types/environment.types.ts
+++ src/types/environment.types.ts
@@ -4,5 +4,6 @@ export type EnvironmentSecrets = {
     AUTH_SECRET: string;
     DATABASE_CONNECTION_STR: string;
     EMAIL_SERVICE_API_KEY: string;
+    EMAIL_SERVICE_SECRET: string;
     NODE_ENV: string;
 };
