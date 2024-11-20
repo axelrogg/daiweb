77ce647d83514aab152484d8f3c6e4528b770320
Axel Rodríguez Chang <axelrogg@icloud.com>
2024-11-20 12:02:18 +0100

    prettify

--- src/lib/actions/user/user-info.ts
+++ src/lib/actions/user/user-info.ts
@@ -16,7 +16,7 @@ export async function userInfo(id?: number | undefined) {
                     session: session,
                 },
                 "No active session was found. No user information can be fetched"
-            )
+            );
             return null;
         }
         userId = Number(session.user.id);
@@ -26,17 +26,17 @@ export async function userInfo(id?: number | undefined) {
                 userId: userId,
             },
             "User ID found from session object"
-        )
+        );
     }
 
     try {
         const userInfo = await user.info(userId);
         log.debug(
             {
-                userInfo: userInfo
+                userInfo: userInfo,
             },
             "User info found from user id"
-        )
+        );
         return userInfo;
     } catch (error: any) {
         log.error(
@@ -46,7 +46,7 @@ export async function userInfo(id?: number | undefined) {
                 stack: error.stack,
             },
             "Error occurred while fetching user info"
-        )
-        return null
+        );
+        return null;
     }
 }
